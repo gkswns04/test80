4c9f9a6aa0104771228ca65e5deb1a97c8bacfb3
minwoo85 <lmwjjang63@naver.com>
2016-04-21 18:20:10 +0900

    .

--- test01/src/bitcamp/pms/controller/BoardController.java
+++ test01/src/bitcamp/pms/controller/BoardController.java
@@ -1,5 +1,6 @@
 package bitcamp.pms.controller;
 
+import java.sql.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,7 +14,12 @@ import bitcamp.pms.util.CommandUtil;
 @RequestMapping("board/") // 매핑정보 일부를 선언한다.
 public class BoardController {  
   private BoardDao boardDao;  
-
+  
+  
+  private void syso() {
+    
+  System.out.println("sss");
+  }
   public void setBoardDao(BoardDao boardDao) {
     this.boardDao = boardDao;
   }
@@ -21,12 +27,15 @@ public class BoardController {
   @RequestMapping("add.do") // 매핑정보 나머지를 선언한다.
   public void add(Scanner keyScan) {    
     Board board = new Board();    
-    System.out.print("제목? ");
-    board.setTitle(keyScan.nextLine());
     System.out.print("내용? ");
     board.setContent(keyScan.nextLine());
+
+    System.out.print("제목? ");
+    board.setTitle(keyScan.nextLine());
     System.out.print("작성자? ");
-    board.setWriter(keyScan.nextLine());    
+    board.setWriter(keyScan.nextLine());
+    board.setCreatedDate(new Date(System.currentTimeMillis()));
+    
     if (CommandUtil.confirm(keyScan, "저장하시겠습니까?")) {         
       try {
         boardDao.insert(board);   
@@ -46,7 +55,7 @@ public class BoardController {
   public void delete(Scanner keyScan) {      
     try {
       this.list();
-      System.out.print("삭제하고 싶은 게시물 번호를 입력하세요. ");
+      System.out.print("삭제하고 싶은 게시판 번호를 입력하세요. ");
       int no = Integer.parseInt(keyScan.nextLine());
       if (CommandUtil.confirm(keyScan, "삭제 하시겠습니까?")) {
         int count = boardDao.delete(no);
@@ -71,10 +80,13 @@ public class BoardController {
   public void list() {
     try {
       List<Board> boards = boardDao.selectList();          
-      System.out.println("저장된 게시물 목록입니다.");
+      System.out.println("저장된 게시판 목록입니다.");
       for (Board board : boards) {
-        System.out.printf("%d번 %s, %s, %s\n", board.getNo(),
-            board.getTitle(), board.getWriter(), board.getCreatedDate());        
+
+        System.out.printf("%d번 %s, %s, %s, %s, %d\n", board.getBno(),
+            board.getTitle(), board.getWriter(), board.getContent(),        
+            board.getCreatedDate(), board.getViews());
+
       }
     } catch (Exception e) {
       throw new RuntimeException("게시판 데이터 로딩 실패!", e);
@@ -86,15 +98,15 @@ public class BoardController {
     
     try { 
       this.list();
-      System.out.print("변경하고 싶은 게시물 번호를 입력하세요. ");
+      System.out.print("변경하고 싶은 게시판 번호를 입력하세요. ");
       int no = Integer.parseInt(keyScan.nextLine());      
       Board board = boardDao.selectOne(no);      
       System.out.printf("제목(%s)? ", board.getTitle());
       board.setTitle(keyScan.nextLine());
       System.out.printf("내용(%s)? ", board.getContent());
       board.setContent(keyScan.nextLine());
-      System.out.print("작성자? ");
-      board.setWriter(keyScan.nextLine());     
+      board.setCreatedDate(new Date(System.currentTimeMillis()));   
+
       
       if (CommandUtil.confirm(keyScan, "변경 하시겠습니까?")) {        
         int count = boardDao.update(board);
