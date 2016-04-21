1dce0a6d5e9dbd181493e2d14159bc0b5d42626d
KIM YANG MO <yangdol2@nate.com>
2016-04-21 10:36:01 +0900

    aaa

--- test01/src/Hello2.java
+++ test01/src/Hello2.java
@@ -2,15 +2,19 @@
 public class Hello2 {
 
   public static void main(String[] args) {
+    System.out.println("양모1");    
+    System.out.println("양모2");
+    
+    System.out.println("양모3");
     System.out.println("Hello!");
     System.out.println("okok");
     System.out.println("nono");
     System.out.println("hi");
     System.out.println("세종대왕!");
     System.out.println("임꺽정!");
-    System.out.println("양모");
+    System.out.println("양모4");
  
-    System.out.println("양모");
+    
     
   }
 
