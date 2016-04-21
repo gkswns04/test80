b4e074f95a465ca9e3a2699a673802a6f86118a0
gkswns04 <gkswns04@naver.com>
2016-04-21 10:28:11 +0900

    .

--- test01/src/Hello2.java
+++ test01/src/Hello2.java
@@ -6,9 +6,10 @@ public class Hello2 {
     System.out.println("okok");
     System.out.println("nono");
     System.out.println("hi");
+    System.out.println("세종대왕!");
+    System.out.println("임꺽정!");
     System.out.println("양모");
-    System.out.println("양모");
-    System.out.println("양모");
+ 
     System.out.println("양모");
     
   }
